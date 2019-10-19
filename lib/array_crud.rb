00bffdc4ff96392a93beba8acfc3a42da3d925e0
Robert M Ricci <rmricci121@gmail.com>
2019-10-19 00:43:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  our_famly.push("GG")
+  our_family = ["Rob", "Monica","Alice", "Margo"]
+  << "GG"
 end
 
 def add_element_to_start_of_array(array, element)
