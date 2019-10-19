70f473e4c12cc4c0738e43a651345ca6b069ac82
Robert M Ricci <rmricci121@gmail.com>
2019-10-19 00:52:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   our_pets=["walnt", "sela", "goldie", "Anna"]
-  our_pets.push("Goldie")
+  our_pets << ("Goldie")
 end
 
 def add_element_to_start_of_array(array, element)
