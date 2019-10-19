9140ef8a3d5c2cdbff3bb50c85bc3024a8cf672e
Robert M Ricci <rmricci121@gmail.com>
2019-10-19 00:44:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   our_family = ["Rob", "Monica","Alice", "Margo"]
-  << "GG"
+  our_family << "GG"
 end
 
 def add_element_to_start_of_array(array, element)
