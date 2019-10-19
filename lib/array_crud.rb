2d73f1a9855659a5ae9987e476fd807d8c168dc1
Robert M Ricci <rmricci121@gmail.com>
2019-10-19 01:00:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,6 +14,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
+  our_family = ["Rob", "Monica","Alice", "Margo"]
+  
   our_family.unshift("walnut")
 end
 
