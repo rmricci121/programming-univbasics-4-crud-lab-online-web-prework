a3c9a54c21c257242d51a5a4ae34c19e92fe372f
Robert M Ricci <rmricci121@gmail.com>
2019-10-19 01:03:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  our_family = ["Rob", "Monica","Alice", "Margo"]
+  our_family = ["Rob", "Monica","Alice", "arrays!"]
   
   our_family.pop
 end
