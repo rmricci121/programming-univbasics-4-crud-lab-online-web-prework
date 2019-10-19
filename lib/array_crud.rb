8c92e26b46fa0ab7b29bd20b0d206cf0bfef3caa
Robert M Ricci <rmricci121@gmail.com>
2019-10-19 01:10:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -38,7 +38,9 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
+  our_family = ["wow", "Monica","Alice", "Margo"]
   
+  our_family[0]
 end
 
 def retrieve_last_element_from_array(array)
